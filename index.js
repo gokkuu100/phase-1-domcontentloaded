7300d9a3cfa37ba31a46c9e4f5119b012abec0ea
goku <wkurts247@gmail.com>
2023-07-03 12:54:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const text = document.querySelector("#text")
+    text.innerHTML += `<p>This is really cool!</p>`
+    console.log(text);
+})
